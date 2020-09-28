4c4157dbabb7e3bdfefff8848faaa54805563ba0
Georg Wilhelm MAIRHUBER <gmairhuber18@htl-grieskirchen.local>
2020-09-28 15:59:08 +0200

    huan

--- src/homework/pkg2/NumberTester.java
+++ src/homework/pkg2/NumberTester.java
@@ -5,41 +5,42 @@
  */
 package homework.pkg2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gmairhuber18
  */
-public class NumberTester{
+public class NumberTester {
 
     String file;
     NumberTest OddEvenTester;
     NumberTest PrimeTester;
     NumberTest PalindromeTester;
-    int i = 1;
-    
+    Scanner sc = new Scanner(System.in);
+
     public NumberTester(String filename) {
-        
+
         this.file = filename;
-        
+
     }
-    
-    public void setOddEvenTester(NumberTest oddtest)
-    {
+
+    public void setOddEvenTester(NumberTest oddtest) {
         this.OddEvenTester = oddtest;
     }
-    
-    public void setPrimeTester(NumberTest primetest)
-    {
+
+    public void setPrimeTester(NumberTest primetest) {
         this.PrimeTester = primetest;
     }
-    
-    public void PalindromeTester(NumberTest palindrometest)
-    {
+
+    public void PalindromeTester(NumberTest palindrometest) {
         this.PalindromeTester = palindrometest;
     }
-    
-    public void testFile()
-    {
-        
+
+    public void testFile() {
+            int anzahl = sc.nextInt();
+            for (int i = 0; i < anzahl; i++) {
+            //switch
+        }
     }
 }
